29ae17ed9787a6d83a9cc7577c283aaba059a666
thbaylson <thbaylson@gmail.com>
2025-04-22 13:46:42 -0400

    Setup an Entity and added a Component. The System is soon to come.

---
+++ Assets/Scripts/Algorithms/DOTS Components/RotateSpeedAuthoring.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+// Components must be structs. They should not contain any functions.
+// All of the properties in a component should be public.
+// This struct does not necessarily need to be in the same script, but it looks more organized this way.
+public struct RotateSpeed : IComponentData
+{
+    public float value;
+}
+
+// Appending "Authoring" to the name is just a convention. But the class name MUST be the same as the file name.
+public class RotateSpeedAuthoring : MonoBehaviour
+{
+    public float value;
+
+    // This placement and naming is also a convention.
+    private class Baker : Baker<RotateSpeedAuthoring>
+    {
+        public override void Bake(RotateSpeedAuthoring authoring)
+        {
+            // We need the Dynamic flag because we intend to move/rotate this object.
+            Entity entity = GetEntity(TransformUsageFlags.Dynamic);
+            AddComponent(entity, new RotateSpeed
+            {
+                value = authoring.value,
+            });
+        }
+    }
+}
